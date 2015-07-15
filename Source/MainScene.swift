f9786c41e05471973b4843b61a6872fb9f2e2e67
Daniel Rubin <danielrubin17@gmail.com>
2015-07-15 16:05:43 -0700

    Working AI

--- Source/MainScene.swift
+++ Source/MainScene.swift
@@ -13,36 +13,43 @@ class MainScene: CCNode, CCPhysicsCollisionDelegate {
     weak var hero: Hero!
     weak var basicEnemy: BasicEnemy!
     weak var gamePhysicsNode: CCPhysicsNode!
-    var jumped = false//move to hero class later
-    var shotCollision = 0
-    var limitShots = 0
-    var heroShots = 0
-    var numTimes = 0
+    weak var enemyNode: CCNode!//All enemies added onto this node
+    var jumped = true//move to hero class later
+    var shotCollision = 0 //Not sure, currently commented out.
+    var limitShots = 0//limits enemies to shooting once per 28 frames
+    var heroShots = 30//limits hero to shooting once per 30 frames (one sec)
+    var enemyArray: [BasicEnemy] = []//array of BasicEnemy's in addEnemy(xCor, yCor)
+    var numBasicEnemies = 0//keeps track of num of enemies.  Add one as starts at 0
     
     func didLoadFromCCB() {
+        numBasicEnemies--
         userInteractionEnabled = true
         gamePhysicsNode.collisionDelegate = self
+        addEnemy(400, yCor: 150)
     }
     
     override func fixedUpdate(delta: CCTime) {
-        if basicEnemy.detectHero(hero.position.y, xCor: hero.position.x, scale: -1) && basicEnemy.scaleX == -1 {
-            generalShot(basicEnemy.position.x, yCor: basicEnemy.position.y, scale: -1)
-        }
-        else if basicEnemy.detectHero(hero.position.y, xCor: hero.position.x, scale: 1) && basicEnemy.scaleX == 1 {
-            generalShot(basicEnemy.position.x, yCor: basicEnemy.position.y, scale: 1)
-        }
-            
-        else {
-            basicEnemy.move()
-            limitShots++
-            if hero.movementState == HeroMovementState.MovingRight {
-                hero.position = CGPoint(x: (hero.position.x + 1), y: hero.position.y)
+        for index in 0...numBasicEnemies {
+            if enemyArray[index].detectHero(hero.position.y, xCor: hero.position.x, scale: -1) && enemyArray[index].scaleX == -1 && limitShots >= 28 {
+                basicEnemyShot(enemyArray[index])
+                limitShots = 0
             }
-            else if hero.movementState == HeroMovementState.MovingLeft {
-                hero.position = CGPoint(x: (hero.position.x - 1), y: hero.position.y)
+            else if enemyArray[index].detectHero(hero.position.y, xCor: hero.position.x, scale: 1) && enemyArray[index].scaleX == 1 && limitShots >= 28 {
+                basicEnemyShot(enemyArray[index])
+                limitShots = 0
             }
+            else {
+                enemyArray[index].move()
+            }
+        }
+        if hero.movementState == HeroMovementState.MovingRight {
+            hero.position = CGPoint(x: (hero.position.x + 1), y: hero.position.y)
+        }
+        else if hero.movementState == HeroMovementState.MovingLeft {
+            hero.position = CGPoint(x: (hero.position.x - 1), y: hero.position.y)
         }
         heroShots++
+        limitShots++
     }
     
     override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
@@ -79,23 +86,23 @@ class MainScene: CCNode, CCPhysicsCollisionDelegate {
     }
 
     func ccPhysicsCollisionPostSolve(pair: CCPhysicsCollisionPair!, falling: Hero!, ground: CCNode!) {
-        if jumped == true && shotCollision == 0 {
+        if jumped == true/* && shotCollision == 0*/ {
             if hero.jump.visible == true {
                 hero.idleAnimate()
             }
             jumped = false
         }
-        shotCollision = 0
+//        shotCollision = 0
     }
     
     func ccPhysicsCollisionPostSolve(pair: CCPhysicsCollisionPair!, basicEnemyCrash: BasicEnemy!, wall: CCNode!) {
-        if basicEnemy.scaleX == -1 {
-            basicEnemy.scaleX = 1
-            basicEnemy.movementState = BasicEnemyState.MovingRight
+        if basicEnemyCrash.scaleX == -1 {
+            basicEnemyCrash.scaleX = 1
+            basicEnemyCrash.movementState = BasicEnemyState.MovingRight
         }
-        else if basicEnemy.scaleX == 1 {
-            basicEnemy.scaleX = -1
-            basicEnemy.movementState = BasicEnemyState.MovingLeft
+        else if basicEnemyCrash.scaleX == 1 {
+            basicEnemyCrash.scaleX = -1
+            basicEnemyCrash.movementState = BasicEnemyState.MovingLeft
         }
     }
     
@@ -103,16 +110,25 @@ class MainScene: CCNode, CCPhysicsCollisionDelegate {
         self.gamePhysicsNode.removeChild(shot)
     }
     
+    func ccPhysicsCollisionPostSolve(pair: CCPhysicsCollisionPair!, basicEnemyCrash name1: BasicEnemy!, basicEnemyCrash: BasicEnemy!) {
+        if basicEnemyCrash.scaleX == -1 {
+            basicEnemyCrash.scaleX = 1
+            basicEnemyCrash.movementState = BasicEnemyState.MovingRight
+        }
+        else if basicEnemyCrash.scaleX == 1 {
+            basicEnemyCrash.scaleX = -1
+            basicEnemyCrash.movementState = BasicEnemyState.MovingLeft
+        }
+    }
     
     func ccPhysicsCollisionPostSolve(pair: CCPhysicsCollisionPair!, basicEnemyCrash: BasicEnemy!, shot: CCParticleSystem!) {
-        basicEnemyCrash.position.x = 200
-        basicEnemyCrash.position.y = 125
+        enemyNode.removeChild(basicEnemyCrash)
     }
     
     func shoot() {
         if heroShots >= 30 {
             heroShots = 0
-            shotCollision++
+//            shotCollision++
             hero.hasShot()
             let explosion = CCBReader.load("Shot") as! CCParticleSystem
             explosion.autoRemoveOnFinish = true
@@ -120,20 +136,24 @@ class MainScene: CCNode, CCPhysicsCollisionDelegate {
             explosion.position.x = (hero.position.x + CGFloat(hero.shotMoveScale))
             gamePhysicsNode.addChild(explosion)
             explosion.physicsBody.applyImpulse(CGPoint(x: hero.shotImpulseScale, y: 0))
-            shotCollision++
+//            shotCollision++
         }
     }
     
-    func generalShot(xCor: CGFloat, yCor: CGFloat, scale: Int) {
-        if limitShots >= 28 {
-            limitShots = 0
-            let explosionEnemy = CCBReader.load("Shot") as! CCParticleSystem
-            explosionEnemy.autoRemoveOnFinish = true
-            explosionEnemy.position.y = (yCor + 5)
-            explosionEnemy.position.x = (xCor + CGFloat(basicEnemy.shotMoveScale))
-            gamePhysicsNode.addChild(explosionEnemy)
-            explosionEnemy.physicsBody.applyImpulse(CGPoint(x: basicEnemy.shotImpulseScale, y: 0))
-        }
-        limitShots++
+    func basicEnemyShot(enemyPos: BasicEnemy) {
+        let explosionEnemy = CCBReader.load("Shot") as! CCParticleSystem
+        explosionEnemy.autoRemoveOnFinish = true
+        explosionEnemy.position.y = (enemyPos.position.y + 5)
+        explosionEnemy.position.x = (enemyPos.position.x + CGFloat(enemyPos.shotMoveScale))
+        gamePhysicsNode.addChild(explosionEnemy)
+        explosionEnemy.physicsBody.applyImpulse(CGPoint(x: enemyPos.shotImpulseScale, y: 0))
+    }
+    
+    func addEnemy(xCor: CGFloat, yCor: CGFloat) {
+        let temporaryBasicEnemy = CCBReader.load("BasicEnemy") as! BasicEnemy
+        enemyArray.append(temporaryBasicEnemy)
+        temporaryBasicEnemy.position = CGPoint(x: xCor, y: yCor)
+        enemyNode.addChild(temporaryBasicEnemy)
+        numBasicEnemies++
     }
 }
